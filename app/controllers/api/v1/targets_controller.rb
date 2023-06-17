b120f70307132e72b26cb957daeff8b2876eb89f
Diegomez-dev <diegoagc@yahoo.es>
2023-06-17 12:15:18 -0500

    jbuilder improvements

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -4,7 +4,6 @@ module Api
       def create
         authorize Target
         @target = current_user.targets.create!(target_params)
-        render json: @target, status: :created
       end
 
       private
