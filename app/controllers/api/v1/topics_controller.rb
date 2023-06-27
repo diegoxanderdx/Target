572b1ed31548c17ec4e5895f332d31c3983ff255
Diego Gomez Chavarria <diegoxanderdx@gmail.com>
2023-06-27 14:23:44 -0500

    minor enhacement in topic controller
    Co-authored-by: Diego Suárez <diexee@gmail.com>

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TopicsController < Api::V1::ApiController
       def index
-        @topics = policy_scope(Topic).include([:icon_url])
+        @topics = policy_scope(Topic).include(:icon_url)
         render json: @topics
       end
     end
