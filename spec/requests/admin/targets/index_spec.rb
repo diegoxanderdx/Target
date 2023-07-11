340826273b64ab036b0c4c0971b5fafa37d995bb
Diegomez-dev <diegoagc@yahoo.es>
2023-07-11 05:33:23 -0500

    change the index spec

--- spec/requests/admin/targets/index_spec.rb
+++ spec/requests/admin/targets/index_spec.rb
@@ -1,8 +1,7 @@
 describe 'Admin see targets', type: :feature do
   let!(:admin_user) { create(:admin_user) }
-  let!(:targets) { create_list(:target, 4) }
-  let(:first_target) { targets.first }
-  let(:last_target) { targets.last }
+  let!(:targets) { create_list(:target, 2) }
+  let!(:last_target) { targets.last }
   subject { visit admin_targets_path }
 
   before { sign_in(admin_user) }
@@ -12,20 +11,7 @@ describe 'Admin see targets', type: :feature do
     expect(page).to have_content('Targets')
   end
 
-  it 'index the first target' do
-    subject
-    expect(page).to have_content(first_target.id)
-    expect(page).to have_content(first_target.topic.name)
-    expect(page).to have_content(first_target.user.full_name)
-    expect(page).to have_content(first_target.title)
-    expect(page).to have_content(first_target.radius)
-    expect(page).to have_content(first_target.latitude)
-    expect(page).to have_content(first_target.longitude)
-    expect(page).to have_content(first_target.created_at.strftime('%B %d, %Y %H:%M'))
-    expect(page).to have_content(first_target.updated_at.strftime('%B %d, %Y %H:%M'))
-  end
-
-  it 'index the last target' do
+  it 'renders the last target' do
     subject
     expect(page).to have_content(last_target.id)
     expect(page).to have_content(last_target.topic.name)
